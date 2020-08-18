6d7a91acaf58b8ab3303ac21b2f18c7c1189667d
Jean Ruiz <jrchamorro95@gmail.com>
2020-08-18 12:40:53 -0600

    Implementacion de template(grayscale de bootstrap) a las vistas

---
+++ public/javascripts/map.js
@@ -0,0 +1,13 @@
+
+var map = L.map('mapid', {
+    center: [12.14301, -86.22379],
+    zoom: 13
+});
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+}).addTo(map)
+
+
+L.marker([12.14258, -86.22402]).addTo(map);
+L.marker([12.1405460, -86.2291840]).addTo(map);
+L.marker([12.1418754, -86.2246273]).addTo(map);
