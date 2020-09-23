7a5e82fe9da9e2d32a6348f7d0735453743a2520
Jean Ruiz <jrchamorro95@gmail.com>
2020-09-23 16:43:57 -0600

    integracion de SendGrid

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,33 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport'); //npm install nodemailer-sendgrid-transport --save
 
-const mailcongif={
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false,
-    auth:{
-        user:'norene.shanahan89@ethereal.email',
-        pass:'JRurCJpPFykdJH8ft8',
+let mailcongif;
+
+if(process.env.NODE_ENV === 'production'){
+    const options ={
+        auth:{
+            api_key: process.env.SENDGRID_API_SECRET
+        }
+    }
+    mailcongif = sgTransport(options);
+}else{
+    if(process.env.NODE_ENV === 'staging'){
+        const options ={
+            auth:{
+                api_key: process.env.SENDGRID_API_SECRET
+            }
+        }
+        mailcongif = sgTransport(options);
+    }else{
+        mailcongif={
+            host: 'smtp.ethereal.email',
+            port: 587,
+            secure: false,
+            auth:{
+                user: process.env.ethereal_user,
+                pass: process.env.ethereal_pwd,
+            }
+        }        
     }
 }
 
