ff3893913f0aa7a1d744b1df30c8b2fe76e8554f
Jean Ruiz <jrchamorro95@gmail.com>
2020-09-26 13:29:10 -0600

    agregamos politicas de privacidad requisito en GD

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var user = require('../models/users');
+const path = require('path');
 
 const logged = require('../controllers/login');//Middelware
 
 /* GET home page. */
 router.get('/',logged.loggedIn, function(req, res) {
-      res.render('index', { title: 'Express',usuario_rol: req.role, validate: true });
+      res.render('index', { title: 'Bicycle Network',usuario_rol: req.role, validate: true });
 });
 
 router.get('/_', function(req, res) {
-    res.render('index', { title: 'Express',validate: true });
+    res.render('index', { title: 'Bicycle Network',validate: true });
+});
+
+router.get('/privacy_policy', function(req, res){
+    res.sendFile(path.resolve('public/privacy_policy.html'));
 });
 
 module.exports = router;
